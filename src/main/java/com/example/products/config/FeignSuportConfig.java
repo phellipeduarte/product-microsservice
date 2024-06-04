2760bde523d3a22f53beecd35df53b90d714ebe5
Natan Fernandes <natanfernandes@protonmail.com>
2024-06-04 00:24:04 -0300

    fix

--- src/main/java/com/example/products/config/FeignSuportConfig.java
+++ src/main/java/com/example/products/config/FeignSuportConfig.java
@@ -6,18 +6,27 @@ import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import javax.naming.Context;
+import javax.naming.Name;
 import javax.naming.spi.ObjectFactory;
 import java.beans.Encoder;
+import java.util.Hashtable;
 
 
-public class FeignSuportConfig {
+/*public class FeignSuportConfig {
     @Bean
     public Encoder multipartFormEncoder() {
-        return new SpringFormEncoder(new SpringEncoder(new ObjectFactory<HttpMessageConverters>() {
+        return new SpringFormEncoder(new SpringEncoder(new ObjectFactory() {
             @Override
+            public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
+                return this;
+            }
+
             public HttpMessageConverters getObject() throws BeansException {
                 return new HttpMessageConverters(new RestTemplate().getMessageConverters());
             }
         }));
     }
 }
+*/
--- src/main/java/com/example/products/services/ImageService.java
+++ src/main/java/com/example/products/services/ImageService.java
@@ -1,15 +1,14 @@
 package com.example.products.services;
 
-import com.example.products.config.FeignSuportConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name = "image-service", url = "http://localhost:8081/api/image", configuration = FeignSuportConfig.class)
+@FeignClient(name = "image-service", url = "http://localhost:8081/api/image")
 public interface ImageService {
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    String uploadImage(@RequestParam("file") MultipartFile file);
+    String uploadImage(@RequestPart("file") MultipartFile file);
 }
