40e1f1351d8dbc11ecdb90b0ea558493a793c8da
phellipeduarte <phellipe.duarte@icloud.com>
2024-06-07 12:11:51 -0300

    Adicionando testes de exceção

--- src/test/java/com/example/products/services/OrderServiceTest.java
+++ src/test/java/com/example/products/services/OrderServiceTest.java
@@ -1,13 +1,13 @@
 package com.example.products.services;
 
-import com.example.products.dtos.EmailRequestDTO;
 import com.example.products.dtos.OrderRequestDTO;
 import com.example.products.enums.PaymentStatusEnum;
+import com.example.products.exceptions.ProductNotFoundException;
+import com.example.products.exceptions.ProductSoldException;
 import com.example.products.models.Order;
 import com.example.products.models.Product;
 import com.example.products.repositories.OrderRepository;
 import com.example.products.repositories.ProductRepository;
-import org.antlr.v4.runtime.atn.SemanticContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -37,21 +37,26 @@ class OrderServiceTest {
     @InjectMocks
     OrderService orderService;
 
+
+    UUID randomUUID;
+    String clientEmail;
+    Product product;
+    Order order;
+
+
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
+
+        randomUUID = UUID.randomUUID();
+        clientEmail = "email@email.com";
+        product = new Product(randomUUID, "iPhone", 100.00, "");
+        order = new Order(clientEmail, PaymentStatusEnum.PENDENTE, product);
     }
 
     @Test
     @DisplayName("Should create a new order.")
     void createOrder() {
-
-        UUID randomUUID = UUID.randomUUID();
-        String clientEmail = "email@email.com";
-
-        Product product = new Product(randomUUID, "iPhone", 100.00, "");
-        Order order = new Order(clientEmail, PaymentStatusEnum.PENDENTE, product);
-
         when(orderRepository.save(order)).thenReturn(order);
         when(productRepository.findById(randomUUID)).thenReturn(Optional.of(product));
 
@@ -60,5 +65,33 @@ class OrderServiceTest {
 
         verify(orderRepository, times(1)).save(createdOrder);
         verify(productRepository, times(1)).findById(randomUUID);
+    }
+
+
+    @Test
+    @DisplayName("Should except not finding a Product.")
+    void exceptProductNotFound() {
+        OrderRequestDTO orderRequestDTO = new OrderRequestDTO(product.getUuid(), clientEmail);
+        try{
+            orderService.createOrder(orderRequestDTO);
+            fail();
+        } catch (ProductNotFoundException productNotFoundException){
+            assertEquals("Product not found.", productNotFoundException.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Should except while trying to buy a sold product.")
+    void exceptProductSold() {
+        when(orderRepository.findByProductUuid(randomUUID)).thenReturn(Optional.of(order));
+        when(productRepository.findById(randomUUID)).thenReturn(Optional.of(product));
+
+        OrderRequestDTO orderRequestDTO = new OrderRequestDTO(product.getUuid(), clientEmail);
+        try{
+            orderService.createOrder(orderRequestDTO);
+            fail();
+        } catch (ProductSoldException productSoldException){
+            assertEquals("Product has already been sold.", productSoldException.getMessage());
+        }
     }
 }
